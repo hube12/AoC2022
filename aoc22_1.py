fd0adbec09563827e011bc098e69bf84cd3bdc24
Neil <h66R10@gmail.com>
2022-12-02 08:28:31 +0100

    Add files via upload

---
+++ aoc22_1.py
@@ -0,0 +1,19 @@
+import os
+from operator import itemgetter
+print(os.getcwd())
+elfs=[]
+with open("../Download/input22_1.txt") as f:
+   
+   current_elf=[]
+   for line in f:
+      line=line.strip()
+      if not line:
+         elfs.append(current_elf)
+         current_elf=[]
+      else:
+         current_elf.append(int(line))
+
+
+print(max([(i,sum(x)) for i,x in enumerate(elfs)],key=itemgetter(1)))
+
+print(sum(x[1] for x in sorted([(i,sum(x)) for i,x in enumerate(elfs)],key=itemgetter(1))[-3:]))
